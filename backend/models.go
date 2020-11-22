d2ef737dd6f051256ad288f4b875d62852458430
kareemshahin <kareem.shahin@gmail.com>
2020-11-21 21:36:44 -0800

    add player struct

--- backend/models.go
+++ backend/models.go
@@ -5,3 +5,10 @@ type Game struct {
 	Name  string `json:"name"`
 	State string `json:"state"`
 }
+
+type Player struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Token string
+	Score int `json:"score"`
+}
