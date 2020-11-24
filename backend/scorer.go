124c896d9919877be14d2bbb423f7992fe46981f
kareemshahin <kareem.shahin@gmail.com>
2020-11-23 19:25:34 -0800

    wild algos

--- backend/scorer.go
+++ backend/scorer.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 const KING = "K"
 const QUEEN = "Q"
 const JACK = "J"
@@ -25,13 +30,68 @@ func calculateBasra(cardPlayed Card, cardsOnTable []Card) HandScore {
 		if card played isFaceCard
 			only pick up face cards
 			basra should be covered in case above
+			jack should pick everything up!
 		else
 			filter out face cards and any cards greater than card played value
 			check for totals for cards won using subsetSum and check for basra (no cards left)
+			check for 7 diamonds
 		end
 	*/
 
 	return HandScore{Score: 0, CardsWon: []Card{}}
+}
+
+/*
+	Given an array of possible totals (from subsetSum), sorts them and finds the best combo
+	of totals that includes the largest set of numbers.
+
+	Example:
+	target sum: 7
+	possible numbers: [1 4 2 3 5 7]
+	possible combo of numbers that sum up to 7 [[1 4 2] [4 3] [2 5] [7]]
+	result of this function: [4 2 3 5 7]  (4 + 3, 5 + 2, 7)
+*/
+func findWinners(totals [][]int, original []int) []int {
+	sort.Slice(totals, func(i, j int) bool { return len(totals[j]) < len(totals[i]) })
+	mostTaken := []int{0}
+
+	for i, v := range totals {
+
+		remainder := Difference(original, v)
+		if (len(original) - len(remainder)) != len(v) {
+			continue
+		}
+		fmt.Println("remaining", remainder)
+		for j := i + 1; j < len(totals); j++ {
+			newRem := Difference(remainder, totals[j])
+			if (len(remainder) - len(newRem)) != len(totals[j]) {
+				continue
+			}
+			remainder = newRem
+		}
+		newTaken := Difference(original, remainder)
+
+		if len(newTaken) > len(mostTaken) {
+			mostTaken = newTaken
+		}
+	}
+	return mostTaken
+}
+
+// Set Difference: A - B
+func Difference(a, b []int) (diff []int) {
+	m := make(map[int]bool)
+
+	for _, item := range b {
+		m[item] = true
+	}
+
+	for _, item := range a {
+		if _, ok := m[item]; !ok {
+			diff = append(diff, item)
+		}
+	}
+	return
 }
 
 func sumArray(values []int) int {
