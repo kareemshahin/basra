d9dfc0c9cbad62d866f5d7af3cf7738ac358e4e1
kareemshahin <kareem.shahin@gmail.com>
2020-12-06 16:00:53 -0800

    test king and queen basra

--- backend/scorer_test.go
+++ backend/scorer_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestCalculateScore(t *testing.T) {
+func TestCalculateScoreJackBasra(t *testing.T) {
 	cardPlayed := Card{
 		Suit:  "C",
 		Rank:  "J",
@@ -25,5 +25,48 @@ func TestCalculateScore(t *testing.T) {
 
 	if len(handScore.CardsWon) != 1 {
 		t.Errorf("Cards won = %d; want 1", len(handScore.CardsWon))
+	}
+}
+
+func TestCalculateScoreFaceCardBasra(t *testing.T) {
+	cardPlayed1 := Card{
+		Suit:  "C",
+		Rank:  "K",
+		Value: 0,
+	}
+
+	cardsOnTable1 := []Card{
+		{Suit: "D", Rank: "K", Value: 0},
+		{Suit: "H", Rank: "K", Value: 0},
+	}
+
+	handScore := CalculateScore(cardPlayed1, cardsOnTable1)
+
+	if handScore.Score != 10 {
+		t.Errorf("Hand score = %d; want 10", handScore.Score)
+	}
+
+	if len(handScore.CardsWon) != 2 {
+		t.Errorf("Cards won = %d; want 2", len(handScore.CardsWon))
+	}
+
+	cardPlayed2 := Card{
+		Suit:  "C",
+		Rank:  "Q",
+		Value: 0,
+	}
+
+	cardsOnTable2 := []Card{
+		{Suit: "D", Rank: "Q", Value: 0},
+	}
+
+	handScore2 := CalculateScore(cardPlayed2, cardsOnTable2)
+
+	if handScore2.Score != 10 {
+		t.Errorf("Hand score = %d; want 10", handScore2.Score)
+	}
+
+	if len(handScore2.CardsWon) != 1 {
+		t.Errorf("Cards won = %d; want 1", len(handScore2.CardsWon))
 	}
 }
